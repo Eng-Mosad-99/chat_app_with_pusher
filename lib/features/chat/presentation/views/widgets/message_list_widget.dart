ec8d42bfd99cb03a4472fc1610aad21345c50c3e
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2026-04-23 09:32:38 -0400

    create MessagesListWidget

---
+++ lib/features/chat/presentation/views/widgets/message_list_widget.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/widgets.dart';
+import '../../../data/models/chat_details_model.dart';
+
+class MessagesListWidget extends StatelessWidget {
+  const MessagesListWidget({
+    super.key,
+    required this.scrollController,
+    required this.chatDetailsModel,
+    required this.currentUser,
+  });
+
+  final ScrollController scrollController;
+  final ChatDetailsModel? chatDetailsModel;
+  final int currentUser;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Expanded(
+        child: ListView.builder(
+            controller: scrollController,
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 30),
+            itemCount: chatDetailsModel!.messages!.length,
+            itemBuilder: (context, i) {
+              Message message = chatDetailsModel!.messages![i];
+              if (currentUser != message.sender!.id) {
+                return ReceiverMsgItemWidget(
+                  message: message,
+                );
+              } else {
+                return SenderMsgItemWidget(
+                  message: message,
+                );
+              }
+            }),
+      ),
+      SendMessageWidget(
+        userId: currentUser,
+        roomId: chatDetailsModel!.chats!.roomId!,
+      ),
+    ]);
+  }
+}
