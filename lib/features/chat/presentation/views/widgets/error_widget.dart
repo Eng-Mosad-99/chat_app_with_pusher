66214f60fd5f9e556b1c8cd0601fda4c7b86c6d2
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2026-04-23 09:35:45 -0400

    create error widget

---
+++ lib/features/chat/presentation/views/widgets/error_widget.dart
@@ -0,0 +1,20 @@
+import 'package:chat_app_with_pusher/core/constants/sizes.dart';
+import 'package:flutter/material.dart';
+
+class ErrorAlertWidget extends StatelessWidget {
+  const ErrorAlertWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: Sizes.getWidth(context),
+      height: Sizes.getHeight(context),
+      child: const Center(
+          child: Icon(
+        Icons.error,
+        color: Colors.redAccent,
+        size: 150,
+      )),
+    );
+  }
+}
